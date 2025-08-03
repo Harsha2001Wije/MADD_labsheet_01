63b8b3bcc14d0fa49741c5c028f50d723bb9f0a5
hasak <harshawijenarayana@gmail.com>
2025-08-03 22:16:12 +0530

    first

---
+++ src/WhoAmI.kt
@@ -0,0 +1,10 @@
+fun whoAmI(value: Any): String {
+        return value::class.simpleName ?: "Unknown"
+}
+
+fun main() {
+        println(whoAmI(42))         // Int
+        println(whoAmI("Hello"))    // String
+        println(whoAmI(3.14))       // Double
+        println(whoAmI(true))       // Boolean
+}
